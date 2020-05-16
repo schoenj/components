2e35fcaab177251d1ef5ca2ddfe6486989292f76
Saithis <saithis88@gmail.com>
2020-05-16 21:29:56 +0200

    refactor(form): deleted some leftovers

--- projects/components/form/public_api.ts
+++ projects/components/form/public_api.ts
@@ -1,14 +1,3 @@
 export { PsFormComponent } from './src/form.component';
 export { PsFormModule } from './src/form.module';
-export {
-  IPsFormCancelParams,
-  IPsFormLoadErrorParams,
-  IPsFormLoadSuccessParams,
-  IPsFormSaveErrorParams,
-  IPsFormSaveParams,
-  IPsFormSaveSuccessParams,
-} from './src/models';
 export { IPsFormDataSource, IPsFormDataSourceConnectOptions, IPsFormButton, IPsFormException } from './src/form-data-source';
-
-// reexport
-export { IPsFormIntlTexts } from '@prosoft/components/core';
